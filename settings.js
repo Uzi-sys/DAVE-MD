ae749c96f98f5987b26dbc14cae6289dd90d0515
giftedsession <oduorid606@gmail.com>
2025-07-25 02:43:56 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'DAVE-MD',
   author: '‎',
   botName: "DAVE-MD",
-  botOwner: 'Dave', // Your name
-  ownerNumber: '254784517274', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'Gifted Dave', // put your name here bro 
+  ownerNumber: '254104260236', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
